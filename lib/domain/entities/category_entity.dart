895dbe20ae5dba407e2ad6d1e9ce8e79d6bda66e
Abhishek <abhisheklacheta@gmail.com>
2025-09-05 23:23:48 +0530

    UI tweaks for firebase options

---
+++ lib/domain/entities/category_entity.dart
@@ -0,0 +1,7 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+/// Provides core implementation and business logic properties.
+/// Provides core implementation and business logic properties.
+/// Provides core implementation and business logic properties.
+class CategoryEntity extends Equatable {

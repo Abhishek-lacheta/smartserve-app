01fada0284922b9b3cfa56a4a72f287f4c93beae
Abhishek <abhisheklacheta@gmail.com>
2025-09-06 16:45:32 +0530

    Formatting fixes

---
+++ lib/core/usecases/usecase.dart
@@ -0,0 +1,4 @@
+import 'package:equatable/equatable.dart';
+import 'package:fpdart/fpdart.dart';
+import 'failure.dart';
+
---
+++ lib/data/repositories/auth_repository.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import '../datasources/firebase_auth_datasource.dart';
+import '../datasources/firestore_datasource.dart';
+import '../models/user_model.dart';
+
+/// Provides core implementation and business logic properties.
+class AuthRepository {

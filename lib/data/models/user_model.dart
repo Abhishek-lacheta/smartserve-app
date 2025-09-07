e84c9ce341719dce19c79f2ce7ae294d34fc56fc
Abhishek <abhisheklacheta@gmail.com>
2025-09-07 11:08:39 +0530

    Fixed alignment issues

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -8,3 +8,9 @@ import '../../domain/entities/user_entity.dart';
 /// Provides core implementation and business logic properties.
 /// Provides core implementation and business logic properties.
 class UserModel extends UserEntity {
+  const UserModel({
+    required super.uid,
+    required super.name,
+    required super.email,
+    super.profileImage,
+  });
---
+++ lib/screens/auth/otp_verification_screen.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+import '../../widgets/custom_button.dart';

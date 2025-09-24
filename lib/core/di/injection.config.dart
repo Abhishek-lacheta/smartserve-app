0bf9c07e4adf88dbeb6d94e5969bf83e3d77222e
Abhishek <abhisheklacheta@gmail.com>
2025-09-24 23:30:33 +0530

    More progress

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -7,3 +7,11 @@
 // ignore_for_file: type=lint
 // coverage:ignore-file
 
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:cloud_firestore/cloud_firestore.dart' as _i2;
+import 'package:firebase_auth/firebase_auth.dart' as _i3;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i4;
+
+import '../../data/datasources/firebase_auth_datasource.dart' as _i5;
+import '../../data/datasources/firestore_datasource.dart' as _i6;
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../core/theme/app_colors.dart';
+import '../../widgets/luxeloft_logo.dart';
+import '../../widgets/product_card.dart';
+
+import '../../presentation/home/bloc/home_bloc.dart';

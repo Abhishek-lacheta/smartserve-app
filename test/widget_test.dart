e200b5d198e1d6b3250af5bedb31cb0844186804
Abhishek <abhisheklacheta@gmail.com>
2025-10-01 23:06:24 +0530

    Fixed build warnings

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,3 +9,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:smartserve/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());

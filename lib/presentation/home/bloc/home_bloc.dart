760e925007ef7c6a7553ef2744fe6361480893c1
Abhishek <abhisheklacheta@gmail.com>
2025-10-02 23:29:53 +0530

    Added login screen

---
+++ lib/presentation/home/bloc/home_bloc.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import '../../../core/usecases/usecase.dart';
+import '../../../domain/usecases/home_usecases.dart';
+import 'home_event.dart';
+import 'home_state.dart';
+
+/// **HomeBloc**
+/// 
+/// Coordinates the retrieval of all data required for the Home Screen.
---
+++ lib/screens/auth/login_screen.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../widgets/luxeloft_logo.dart';
+import '../../widgets/custom_button.dart';

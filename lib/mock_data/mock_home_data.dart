e083f8791298254b315164b092360d41747f3141
Abhishek <abhisheklacheta@gmail.com>
2025-10-05 12:58:12 +0530

    More progress

--- lib/mock_data/mock_home_data.dart
+++ lib/mock_data/mock_home_data.dart
@@ -6,3 +6,7 @@ import 'package:flutter/material.dart';
 /// Provides core implementation and business logic properties.
 class MockData {
   static final List<String> topCategories = [
+    'Recommend',
+    'Cell Phone',
+    'Car Products',
+    'Department Store',
---
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import '../../core/theme/app_colors.dart';
+import '../auth/login_screen.dart';
+

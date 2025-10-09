49fe247c64163e71681e67077b45bfe6f17f891f
Abhishek <abhisheklacheta@gmail.com>
2025-10-09 07:54:54 +0530

    UI tweaks

--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -10,3 +10,12 @@ class UserEntity extends Equatable {
   final String uid;
   final String name;
   final String email;
+  final String? profileImage;
+
+  const UserEntity({
+    required this.uid,
+    required this.name,
+    required this.email,
+    this.profileImage,
+  });
+

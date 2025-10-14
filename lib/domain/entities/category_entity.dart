16e6f196ab591b4c12df561d215edffdc19a4be9
Abhishek <abhisheklacheta@gmail.com>
2025-10-14 22:25:15 +0530

    Fixed alignment issues

--- lib/domain/entities/category_entity.dart
+++ lib/domain/entities/category_entity.dart
@@ -8,3 +8,9 @@ class CategoryEntity extends Equatable {
   final String id;
   final String title;
   final String iconName;
+
+  const CategoryEntity({
+    required this.id,
+    required this.title,
+    required this.iconName,
+  });

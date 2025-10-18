fb7fc8bae3d6b29e28949ec1b706bc0b31d3bf65
Abhishek <abhisheklacheta@gmail.com>
2025-10-18 13:32:15 +0530

    Fixed build warnings

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -2,3 +2,8 @@ import 'package:equatable/equatable.dart';
 import 'package:fpdart/fpdart.dart';
 import 'failure.dart';
 
+/// Base interface for UseCases in the application.
+/// [Type] is the return type of the UseCase.
+/// [Params] are the parameters required to execute the UseCase.
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);

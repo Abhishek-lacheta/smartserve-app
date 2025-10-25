14c3a0d363f38605f1651730ec143d725a1a3a20
Abhishek <abhisheklacheta@gmail.com>
2025-10-25 11:52:16 +0530

    Minor bug fixes

--- lib/mock_data/mock_home_data.dart
+++ lib/mock_data/mock_home_data.dart
@@ -10,3 +10,13 @@ class MockData {
     'Cell Phone',
     'Car Products',
     'Department Store',
+    'Computer',
+  ];
+
+  static final List<Map<String, dynamic>> categories = [
+    {'title': 'Beauty', 'icon': Icons.face},
+    {'title': 'Offers', 'icon': Icons.local_offer},
+    {'title': 'Fashion', 'icon': Icons.checkroom},
+    {'title': 'Home', 'icon': Icons.home},
+    {'title': 'Shirt', 'icon': Icons.dry_cleaning},
+    {'title': 'Woman Bag', 'icon': Icons.shopping_bag},

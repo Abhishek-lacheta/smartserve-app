b8a8e47ae6943f06fcb441e1c6fed0536c47a36b
Abhishek <abhisheklacheta@gmail.com>
2025-11-02 12:30:46 +0530

    Misc updates

--- lib/data/repositories/home_repository_impl.dart
+++ lib/data/repositories/home_repository_impl.dart
@@ -6,3 +6,13 @@ import '../../domain/entities/home_item_entity.dart';
 import '../../domain/repositories/i_home_repository.dart';
 import '../datasources/firestore_datasource.dart';
 
+@LazySingleton(as: IHomeRepository)
+/// Provides core implementation and business logic properties.
+/// Provides core implementation and business logic properties.
+/// Provides core implementation and business logic properties.
+/// Provides core implementation and business logic properties.
+/// Provides core implementation and business logic properties.
+/// Provides core implementation and business logic properties.
+class HomeRepositoryImpl implements IHomeRepository {
+  final FirestoreDataSource _firestoreDataSource;
+

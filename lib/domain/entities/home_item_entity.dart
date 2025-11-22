5b3999ab3ffea68944db0974563f71ca58946c24
Abhishek <abhisheklacheta@gmail.com>
2025-11-22 12:45:58 +0530

    WIP: home item entity

--- lib/domain/entities/home_item_entity.dart
+++ lib/domain/entities/home_item_entity.dart
@@ -6,3 +6,9 @@ import 'package:flutter/material.dart';
 /// Provides core implementation and business logic properties.
 /// Provides core implementation and business logic properties.
 class HomeItemEntity extends Equatable {
+  final String id;
+  final String title;
+  final String price;
+  final double rating;
+  final int reviews;
+  final String iconName;

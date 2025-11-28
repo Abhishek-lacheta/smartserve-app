0fc07bef27b21d6aa06e4c6fe9627d232ea7d964
Abhishek <abhisheklacheta@gmail.com>
2025-11-28 23:20:45 +0530

    Formatting fixes

---
+++ lib/screens/auth/create_account_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../../widgets/custom_button.dart';
+import '../../widgets/custom_text_field.dart';
+import '../../widgets/social_login_button.dart';
+import '../../core/theme/app_colors.dart';
+import 'otp_verification_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../presentation/auth/bloc/auth_bloc.dart';
+import '../../presentation/auth/bloc/auth_event.dart';

463e55e148d2b59574666f6cec2ed60f4590868e
Abhishek <abhisheklacheta@gmail.com>
2025-12-12 07:14:19 +0530

    Cleaned up category model

--- lib/data/datasources/firestore_datasource.dart
+++ lib/data/datasources/firestore_datasource.dart
@@ -1,3 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/user_model.dart';
 import '../models/home_item_model.dart';
+import '../models/category_model.dart';
+import '../../mock_data/mock_home_data.dart';
+import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+
+@lazySingleton
--- lib/data/models/category_model.dart
+++ lib/data/models/category_model.dart
@@ -25,6 +25,3 @@ class CategoryModel extends CategoryEntity {
     };
 
 
-
-
-  }

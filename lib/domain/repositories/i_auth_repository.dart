963f5788788841162016d79f2831757ae0d80c6a
Abhishek <abhisheklacheta@gmail.com>
2026-01-05 08:17:48 +0530

    Minor bug fixes

--- lib/domain/repositories/i_auth_repository.dart
+++ lib/domain/repositories/i_auth_repository.dart
@@ -3,3 +3,9 @@ import '../../core/error/failure.dart';
 import '../entities/user_entity.dart';
 
 abstract class IAuthRepository {
+  Stream<UserEntity?> get authStateChanges;
+  UserEntity? get currentUser;
+  
+  Future<Either<Failure, UserEntity>> signInWithGoogle();
+  Future<Either<Failure, void>> signOut();
+

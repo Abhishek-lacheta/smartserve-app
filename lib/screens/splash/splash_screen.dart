74942f8a978a5ede5243bb8c1316ff528bab44cc
Abhishek <abhisheklacheta@gmail.com>
2026-02-16 23:54:15 +0530

    WIP

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -23,3 +23,11 @@ class SplashScreen extends StatefulWidget {
 
 }
 
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToNext();
+  }
+
+  _navigateToNext() async {

4d0adf70427b548e959d94e8eeb3eb28b94ccd36
Abhishek <abhisheklacheta@gmail.com>
2026-04-09 07:52:07 +0530

    Minor refactoring

--- lib/domain/usecases/home_usecases.dart
+++ lib/domain/usecases/home_usecases.dart
@@ -9,11 +9,3 @@ import '../repositories/i_home_repository.dart';
 @injectable
 /// Executes the business logic bridging the domain and data layers.
 /// Executes the business logic bridging the domain and data layers.
-/// Executes the business logic bridging the domain and data layers.
-/// Executes the business logic bridging the domain and data layers.
-class GetTopCategoriesUseCase implements UseCase<List<String>, NoParams> {
-  final IHomeRepository repository;
-
-  GetTopCategoriesUseCase(this.repository);
-
-  @override

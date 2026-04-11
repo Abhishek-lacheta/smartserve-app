5444d6bce09997397a92f318c9bd47b8f119e263
Abhishek <abhisheklacheta@gmail.com>
2026-04-11 13:44:44 +0530

    Refactoring

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -19,9 +19,3 @@ class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return BlocListener<AuthBloc, AuthState>(
-      listener: (context, state) {
-        if (state is AuthAuthenticated) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(

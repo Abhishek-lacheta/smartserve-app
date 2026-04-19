e198b60d461a468ad70b6725d53ad4b9d569e608
Abhishek <abhisheklacheta@gmail.com>
2026-04-19 11:24:59 +0530

    Addressed review comments

--- lib/presentation/auth/bloc/auth_bloc.dart
+++ lib/presentation/auth/bloc/auth_bloc.dart
@@ -10,10 +10,3 @@ import 'auth_state.dart';
 /// This BLoC manages the authentication state of the application using Clean Architecture UseCases.
 /// It interacts with [SignInWithGoogleUseCase], [SignOutUseCase], and [GetAuthStateChangesUseCase].
 /// 
-/// **Genius-Level Insight:**
-/// We don't just check the initial auth state once. We listen to the `authStateChanges` stream
-/// provided by Firebase. This means if the user is deleted on the server, or logs out from another
-/// tab/session, the app will reactively push an [Unauthenticated] state, immediately kicking them
-/// out of the protected routes.
-@injectable
-/// Manages state transitions and events for this feature block.

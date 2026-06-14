cdae08e741e669d8c4767bac78254fbba1c9a884
Abhishek <abhisheklacheta@gmail.com>
2026-06-14 17:29:37 +0530

    Formatting fixes

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -67,3 +67,12 @@ class ProductCard extends StatelessWidget {
                     ),
                     child: const Icon(
                       Icons.favorite_border,
+                      size: 16,
+                      color: AppColors.textPrimary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Details Area

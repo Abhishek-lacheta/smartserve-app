0ddf56d16beb1ffa5a2c0ee586a3df71ca447bfa
Abhishek <abhisheklacheta@gmail.com>
2026-06-17 22:50:51 +0530

    Optimized some logic

--- lib/presentation/home/bloc/home_bloc.dart
+++ lib/presentation/home/bloc/home_bloc.dart
@@ -42,9 +42,3 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     final results = await Future.wait([
       _getTopCategories(NoParams()),
       _getCategories(NoParams()),
-      _getHomeItems(NoParams()),
-    ]);
-
-    final topCategoriesResult = results[0] as dynamic; // Either<Failure, List<String>>
-    final categoriesResult = results[1] as dynamic; // Either<Failure, List<CategoryEntity>>
-    final itemsResult = results[2] as dynamic; // Either<Failure, List<HomeItemEntity>>

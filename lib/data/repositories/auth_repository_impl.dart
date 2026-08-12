e39f61121241fd1a1742e41518ae557050e25e4e
Abhishek <abhisheklacheta@gmail.com>
2026-08-12 08:29:08 +0530

    Code cleanup for production

--- lib/data/repositories/auth_repository_impl.dart
+++ lib/data/repositories/auth_repository_impl.dart
@@ -66,3 +66,6 @@ class AuthRepositoryImpl implements IAuthRepository {
         await _firestoreDataSource.saveUser(userModel);
         return Right(userModel);
       } else {
+        return const Left(AuthFailure('Google Sign In was canceled.'));
+      }
+    } catch (e) {
